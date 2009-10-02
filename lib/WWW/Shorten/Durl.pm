def383c5d74001dfdf316ef4c396f97021e1fae2
leejj <leejj@leejj-desktop.(none)>
2009-10-02 10:14:07 +0900

    initial commit

---
+++ lib/WWW/Shorten/Durl.pm
@@ -0,0 +1,33 @@
+package WWW::Shorten::Durl;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+WWW::Shorten::Durl -
+
+=head1 SYNOPSIS
+
+  use WWW::Shorten::Durl;
+
+=head1 DESCRIPTION
+
+WWW::Shorten::Durl is
+
+=head1 AUTHOR
+
+JEEN E<lt>jeen@perl.krE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
